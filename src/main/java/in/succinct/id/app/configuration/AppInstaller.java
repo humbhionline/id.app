8cce2cedd9fa27c0b0da4036e425c02dabbd049e
Venky <venkatramanm@gmail.com>
2024-06-12 16:39:27 +0530

    Refactoring

--- src/main/java/in/succinct/id/app/configuration/AppInstaller.java
+++ src/main/java/in/succinct/id/app/configuration/AppInstaller.java
@@ -1,10 +1,12 @@
 package in.succinct.id.app.configuration;
 
 import com.venky.swf.configuration.Installer;
-public class AppInstaller implements Installer{
 
-  public void install() {
-    
-  }
+public class AppInstaller implements Installer {
+
+    public void install() {
+
+    }
+
 }
 
